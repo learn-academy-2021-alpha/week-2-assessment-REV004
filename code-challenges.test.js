83e097e7a525799cf440d77e57b762b685d5e3f1
REV004 <rsier004@gmail.com>
2021-02-22 08:53:21 -0800

    need to finish testing

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -17,78 +17,114 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var num1 = 15
-// Expected output: "15 is divisible by three"
+// var num1 = 15
+// // Expected output: "15 is divisible by three"
 
-var num2 = 0
-// Expected output: "0 is divisible by three"
+// var num2 = 0
+// // Expected output: "0 is divisible by three"
 
-var num3 = -7
-// Expected output: "-7 is not divisble by three"
+// var num3 = -7
+// // Expected output: "-7 is not divisble by three"
 
+// const divider = (num) => {
+//     if (num % 3 === 0)
+//     return `${num} is divisble by three`
+// }else{
+//     return `${num} is not divisible by three`
+// }
 
-
-
+// console.log(divider(num1))
+// console.log(divider(num2))
+// console.log(divider(num3))
 
 // b) Create the function that makes the test pass.
-
-
-
-
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"];
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
 
-var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+var randomNouns2 = [
+	"temperature",
+	"database",
+	"chopsticks",
+	"mango",
+	"deduction",
+];
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
 
+// const capitalLetters = (array) => {
+// 	let newArray = [];
 
+// 	for (let index = 0; index < array.length; index++) {
+// 		newArray.push(array[index].toUpperCase());
+// 	}
 
+// 	return newArray;
+// };
 
+// console.log(capitalLetters(randomNouns1));
+// console.log(capitalLetters(randomNouns2));
 
 // b) Create the function that makes the test pass.
-
-
-
-
 
 // --------------------3) Create a function that takes in an array of mixed data types and returns an array with ONLY NUMBERS sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var mixedDataArray1 = [true, 8, "hello", -8, null, 0, 46, 59, 107, "hey!"]
+var mixedDataArray1 = [true, 8, "hello", -8, null, 0, 46, 59, 107, "hey!"];
 // Expected output: [-8, 0, 8, 46, 59, 107]
 
-var mixedDataArray1 = [3, "yo!", 94, -9, false, 0, 18, "hola!"]
-// Expected output: [-9, 0, 3, 18, 94]
+var mixedDataArray1 = [3, "yo!", 94, -9, false, 0, 18, "hola!"];
+// Expected output: [-9, 0, 3, 18, 94]\
 
+const gimmeNums = (array) => {
+	let sortFilter = array.filter((value) => {
+		return typeof value === "number";
+	});
+	return sortFilter.sort(function (a, b) {
+		return a - b;
+	});
+};
 
-
-
+console.log(gimmeNums(mixedDataArray1));
 
 // b) Create the function that makes the test pass.
-
-
-
-
 
 // --------------------4) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var vowelTester1 = "learn"
-// Expected output: 1
-var vowelTester2 = "academy"
-// Expected output: 0
-var vowelTester3 = "challenge"
-// Expected output: 2
+// var vowelTester1 = "learn";
+// // Expected output: 1
+// var vowelTester2 = "academy";
+// // Expected output: 0
+// var vowelTester3 = "challenge";
+// // Expected output: 2
 
+// const vowelFinder = (string) => {
+// 	let vowelsFinder = ["a", "e", "i", "o", "u"];
+// 	let newArray = string.split("");
+// 	let final = 0;
+// 	for (let index = 0; index < newArray.length; index++) {
+// 		if (
+// 			(final < 0 && newArray[index] === "a") ||
+// 			newArray[index] === "e" ||
+// 			newArray[index] === "i" ||
+// 			newArray[index] === "o" ||
+// 			newArray[index] === "u"
+// 		) {
+// 			final = newArray.indexOf(newArray[index]);
+// 		}
+// 	}
+// 	return final;
+// };
+// //for this one it gives me the first output as correct but for some reason I cant get it to give me other ones *brain explodes* *help!*
 
-
-
+// console.log(vowelFinder(vowelTester1));
+// console.log(vowelFinder(vowelTester2));
+// console.log(vowelFinder(vowelTester3));
 
 // b) Create the function that makes the test pass.
--- interview-questions.md
+++ interview-questions.md
@@ -8,58 +8,52 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Why would you use the method super()?
 
-  Your answer:
+Your answer:
 
-  Researched answer:
-
-
+Researched answer:The super keyword is used to access and call functions on an object's parent. The super. prop and super[expr] expressions are valid in any method definition in both classes and object literals.
 
 2. What is a virtual DOM?
 
-  Your answer:
+Your answer: its a way to see the code ?
 
-  Researched answer:
-
-
+Researched answer:The virtual DOM (VDOM) is a programming concept where an ideal, or “virtual”, representation of a UI is kept in memory and synced with the “real” DOM by a library such as ReactDOM.
 
 3. When creating a basic (stateless) class component in React, what are the necessary elements needed to display "Hello World" in the browser?
 
-  Your answer:
+Your answer:
 
-  Researched answer:
-
-
+Researched answer:
 
 4. What is JSX?
 
-  Your answer:
+Your answer: a language that uses react to communicate
 
-  Researched answer:
+Researched answer:
 
+SX stands for JavaScript XML.
 
+JSX allows us to write HTML in React.
+
+JSX makes it easier to write and add HTML in React.
 
 5. What is state in React?
 
-  Your answer:
+Your answer:something that allows us to get the information from a property
 
-  Researched answer:
-
-
+Researched answer:Eeact components has a built-in state object the state object is where you store property values that belongs to the component. When the state object changes, the component re-renders.
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+Your answer:
 
-  Researched answer:
-
-
+Researched answer: Hoisting is JS's default behavior of defining all the declarations at the top of the scope before code execution
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-- React lifecycle methods
-- React props
-- event.preventDefault()
-- DOM events
-- MVC
+- React lifecycle methods :You can think of React lifecycle methods as the series of events that happen from the birth of a React component to its death. Every component in React goes through a lifecycle of events. I like to think of them as going through a cycle of birth, growth, and death.
+- React props:Props are arguments passed into React components. Props are passed to components via HTML attributes.
+- event.preventDefault():The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur. For example, this can be useful when: Clicking on a "Submit" button, prevent it from submitting a form.
+- DOM events: DOM events allow JavaScript to register different event handlers on elements in an HTML document.
+- MVC:Model–view–controller is a software design pattern commonly used for developing user interfaces that divides the related program logic into three interconnected elements.
